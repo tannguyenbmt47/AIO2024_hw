a7311b4be8fd6fb66592a3093e154906470abf3d
tannguyenbmt47 <nddtan2011@gmail.com>
2024-06-12 14:12:50 +0700

    pep8 extension added

--- Module1/week2/1_Sliding_window.py
+++ Module1/week2/1_Sliding_window.py
@@ -7,20 +7,22 @@ Input: num_list = [3, 4, 5, 1, -44 , 5 ,10, 12 ,33, 1] với k=3
 Output: [5, 5, 5, 5, 10, 12, 33, 33]
 """
 
+
 def find_max_sliding_window(num_list: list[int], k: int):
     result = []
-    
+
     for i in range(len(num_list) - k + 1):
         window = []
         for j in range(k):
             window.append(num_list[i+j])
 
         result.append(max(window))
-    
+
     return result
 
+
 if __name__ == "__main__":
-    num_list = [3, 4, 5, 1, -44 , 5 ,10, 12 ,33, 1]
+    num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
     k = 3
 
     result = find_max_sliding_window(num_list, k)
